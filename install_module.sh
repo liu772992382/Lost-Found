2301dba33f3a961ef2de7c90611344897ebc48d9
gru <772992382@qq.com>
2016-09-10 10:18:47 +0800

    add install.sh

---
+++ install_module.sh
@@ -0,0 +1,21 @@
+#pip
+sudo apt-get install python-pip
+
+#mysql and mysqldb
+sudo apt-get install mysql-server
+sudo apt-get python-setuptools libmysqld-dev python-dev
+sudo pip install mysql-python
+
+#requests
+sudo pip install requests
+
+#flask
+sudo pip install flask
+sudo pip install flask-whooshalchemy
+
+#pillow
+sudo pip install pillow
+
+#crypto
+sudo pip install pycrypto
+sudo apt-get install m2crypto
